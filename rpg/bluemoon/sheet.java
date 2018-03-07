b3debcf908855737b8f5e92e5f18147f2b164c20
Guilherme <guilherme1301703@fapi-pinhais.edu.br>
2018-03-07 18:33:25 -0300

    add sheet for bluemoon rpg, and start the project!

---
+++ rpg/bluemoon/sheet.java
@@ -0,0 +1,23 @@
+package rpg.bluemoon;
+
+public class sheet {
+    String name;
+
+    //fhysique traits
+    int strength;
+    int dexterity;
+    int endurance;
+    int vitality;
+
+    //mind traits
+    int willpower;
+    int inteligence;
+    int wisdom;
+    int insight;
+
+    //Persona traits
+    int charm;
+    int dominion;
+    int leadership;
+    int faith;
+}
